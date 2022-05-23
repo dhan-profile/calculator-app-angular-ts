4c659bd45b4ffcf2e19c0b813aa9aaf65dcead46
DhanushKrishnan <dhanushkrish54@gmail.com>
2022-05-23 08:08:40 +0530

    Calc-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calc';
+  title = 'calculator';
+
+  
+  panel1:any = null;
+  input1:any;
+  input2:any;
+  num1!: number;
+  num2!: number;
+  addition:any;
+  addclick:boolean = false;
+  number1(event: any){
+    this.num1 = event.target.value; 
+  }
+  number2(event: any){
+    this.num2 = event.target.value; 
+  }
+  add(){
+    debugger;
+    this.addclick = true;
+    this.addition = this.num1+this.num2;
+  }
+
+  total(){
+    debugger;
+    if(this.addclick == true){
+      this.panel1 = this.addition;
+    }
+    console.log(this.addition);
+  }
+  reset(){
+    this.panel1="";
+  }
+
+  restrict(event: any) {
+    let code = event.key.charCodeAt(0);
+    if ((code >= 48 && code <= 57) || (code >= 96 && code <= 110)) { }
+    else {
+      event.preventDefault();
+      return;
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input'
+import { MatLabel } from '@angular/material/form-field/label';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
