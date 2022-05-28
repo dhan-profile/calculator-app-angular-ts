6c7d4a05c98790bc488f1d0e528a4157dbe14c86
Dhanush <dhanushkrish45@gmail.com>
2022-05-28 12:50:46 +0530

    Added responsiveness for mobile

--- src/app/manual/manual.component.ts
+++ src/app/manual/manual.component.ts
@@ -14,10 +14,13 @@ multi1!: number;
 multi2!: number;
 div1!: number;
 div2!: number;
+mod1!: number;
+mod2!: number;
 addResult: number = 0;
 subResult: number = 0;
 multiResult: number = 0;
 divResult: number = 0;
+modResult: number = 0;
 constructor() { }
 
 ngOnInit(): void {
@@ -38,5 +41,26 @@ ngOnInit(): void {
   div(){
     this.divResult = +this.div1 / +this.div2;
     return this.divResult;
+  }
+  mod(){
+    this.modResult = +this.mod1 % +this.mod2;
+    return this.modResult;
+  }
+  reset(){
+    this.add1 = 0;
+    this.add2 = 0;
+    this.sub1 = 0;
+    this.sub2 = 0;
+    this.multi1 = 0;
+    this.multi2 = 0;
+    this.div1 = 0;
+    this.div2 = 0;
+    this.mod1 = 0;
+    this.mod2 = 0;
+    this.addResult = 0
+    this.subResult = 0
+    this.multiResult = 0
+    this.divResult = 0
+    this.modResult = 0
   }
 }
